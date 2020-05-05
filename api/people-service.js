37080c8051b1cd0677bfbea0109dc6162f838f88
Khadija781 <49786410+Khadija781@users.noreply.github.com>
2020-05-05 17:14:17 +0200

    Update people-service.js

--- api/people-service.js
+++ api/people-service.js
@@ -5,11 +5,31 @@ module.exports = class PeopleService {
         this.peoples = JSON.parse(fs.readFileSync(__dirname + '/people.json', 'utf8'));
     }
 
+
     updatePeople(id, people) {
-        // To be implemented!
+        const e = -1;
+        const peopleIndex = this.peoples.findIndex(
+            people => people.id === id
+        );
+        if(peopleIndex === e)return Promise.reject('invalid');
+
+        this.peoples[peopleIndex] = people;
+        return {isModified: true};
     }
     
     getPeople(filters) {
-        // To be implemented!
+        const a = 0;
+        console.log(Object.keys(filters).length);
+        if(Object.keys(filters).length === a)
+        {
+            return this.peoples;
+        }
+        else
+        {
+            const peopleIndex = this.peoples.filters(
+                people => people[Object.keys(filters)[0]]===filters[Object.keys(filters)[0]]
+            );
+            return peopleIndex;
+        }
     }
 }
